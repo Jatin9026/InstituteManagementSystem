9f1e2ec293a35f21ef7d4538b65d928851b2977b
jatin <jatingupta918306@gmail.com>
2025-07-11 21:55:27 +0530

    Working on dashboard

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import "../components/style.css";
+import SideNav from "./SideNav";
+import { Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="dashboard-main-container">
+      <div className="dashboard-container">
+     <SideNav/>
+        <div className="main-container">
+          <div className="top-bar">
+            <div className="logo-container">
+              <img src={require("../assets/logo192.png")} />
+            </div>
+            <div>
+              <h1>
+              Infinity learninig Institute
+              </h1>
+              <button>logout</button>
+            </div>
+          </div>
+        <Outlet/>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
